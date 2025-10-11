51502c297b90f7e393945d46e7b077d16209f89a
Arnab <arnabmandal661@gmail.com>
2025-10-11 18:04:04 +0530

    feat(ui): add initial static structure for Badge component

---
+++ src/components/ui/Badge/Badge.tsx
@@ -0,0 +1,12 @@
+export const Badge = () => {
+    return (<>
+            <div className="badge badge-primary badge-secondary">
+            <span>
+
+            </span>
+                <button className="btn btn-ghost btn-xs">
+                    ×
+                </button>
+            </div>
+        </>);
+};

58248463c643409b84d9d7a2bd3cc9b7bad11a9c
Arnab <arnabmandal661@gmail.com>
2025-10-29 17:01:59 +0530

    add template for header component

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -2,6 +2,30 @@ export default function Header() {
     return (
         <>
 
+            <header className="navbar bg-base-100 shadow-lg">
+                <div className="flex-none navbar-start">
+                    <a className="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                             className="inline-block h-5 w-5 stroke-current">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div className="flex-1 navbar-center">
+                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                </div>
+                <div className="flex-none navbar-end">
+                    <button className="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                             className="inline-block h-5 w-5 stroke-current">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+                        </svg>
+                    </button>
+                </div>
+            </header>
+
         </>
     );
 }

219f926a7c1a4941c70d95fdfed130d205e82866
rohit3171999 <rohit3171999@gmail.com>
2025-10-30 17:07:06 +0530

    remove redundant fragment

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -1,19 +1,13 @@
 export default function Card() {
     return (
-        <>
-            <div className="card card-bordered bg-base-100 shadow-xl">
-                <div className="card-body">
-
-
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-
+        <div className="card card-bordered bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h2 className="card-title">Card Title</h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
                 </div>
-
             </div>
-        </>
+        </div>
     );
 }

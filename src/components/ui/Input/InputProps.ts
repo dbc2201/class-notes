44e10791911c57b9076011e418b94ea7b37ebb3c
Arnab <arnabmandal661@gmail.com>
2025-10-31 01:47:47 +0530

    made changes in props

--- src/components/ui/Input/InputProps.ts
+++ src/components/ui/Input/InputProps.ts
@@ -4,9 +4,9 @@ export interface InputProps {
   label: string;
   type: string;
   value: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  placeholder?: string;
+  onChange: (e: ChangeEvent) => void;
+  placeholder: string;
   error?: string;
-  disabled?: boolean;
-  required?: boolean;
+  disabled: boolean;
+  required: boolean;
 }

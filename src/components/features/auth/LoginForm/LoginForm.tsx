8a9a4d4a79a9f5c32b32d85b82ea2baed8603ad1
Arnab <arnabmandal661@gmail.com>
2025-10-31 17:11:01 +0530

    add props in login form

--- src/components/features/auth/LoginForm/LoginForm.tsx
+++ src/components/features/auth/LoginForm/LoginForm.tsx
@@ -1,7 +1,41 @@
-export function LoginForm() {
+import {Button} from "../../../ui/Button/Button.tsx";
+import {Input} from "../../../ui/Input/Input.tsx";
+import type LoginFormProps from "./LoginFormProps.ts";
+
+export function LoginForm(props: LoginFormProps) {
     return (
         <>
-
+            <div className="card card-bordered bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">{props.cardTitle}</h2>
+                    <div>
+                        <Input label={"email"}
+                               type={"email"}
+                               value={""}
+                               onChange={() => {
+                               }}
+                               placeholder={""}
+                               error={""}
+                               disabled={false}
+                               required={false}>
+                        </Input>
+                        <Input label={"password"}
+                               type={"password"}
+                               value={""}
+                               onChange={() => {
+                               }}
+                               placeholder={""}
+                               error={""}
+                               disabled={false}
+                               required={false}>
+                        </Input>
+                    </div>
+                    <div className="card-actions justify-end">
+                        <Button label={props.cardButtonLabel} variant="primary" type="submit"></Button>
+                    </div>
+                    <div>{props.error && <span className="text-error text-sm mt-1">{props.error}</span>}</div>
+                </div>
+            </div>
         </>
     );
 }

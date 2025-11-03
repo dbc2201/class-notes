227174deb8c495666f9cbd1b8c02fd336da6baf6
rohit3171999 <rohit3171999@gmail.com>
2025-11-03 08:16:09 +0530

    fix spacing and add missing semicolon in NotFoundProps.ts

--- src/components/NotFound/NotFoundProps.ts
+++ src/components/NotFound/NotFoundProps.ts
@@ -1,7 +1,7 @@
-export default interface NotFoundProps{
+export default interface NotFoundProps {
     title: string;
     message: string;
-    showButton: boolean
+    showButton: boolean;
     onButtonClick: ()=> void;
     cardButtonLabel: string;
 }

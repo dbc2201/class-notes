4f668fa5edc7e93c1a9ad8ca3e5330a0498a45f4
rohit3171999 <rohit3171999@gmail.com>
2025-12-25 16:53:14 +0530

    feat: add LoginForm to LoginPage
    - Import and render LoginForm component
    - Wrap form in a centered container layout

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,16 @@
+import {LoginForm} from "../../components/features/auth/LoginForm/LoginForm.tsx";
+
 export function LoginPage() {
     return (
-        <></>
+        <div className="min-h-screen flex items-center justify-center bg-base-200">
+            <LoginForm
+                cardTitle="Login"
+                cardButtonLabel="Sign In"
+                isLoading={false}
+                error=""
+                onLogin={()=>{}}
+                onError={()=>{}}
+            />
+        </div>
     );
 }

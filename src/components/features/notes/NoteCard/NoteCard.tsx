58e5b3f2db8ca50038831624f6633943b9ec7852
rohit3171999 <rohit3171999@gmail.com>
2026-01-01 14:36:33 +0530

    feat(NoteCard): implement initial component structure and props
    - Add imports for Button, TagBadge, and NoteCardProps
    - Update NoteCard to accept props
    - Add base card layout with DaisyUI classes

--- src/components/features/notes/NoteCard/NoteCard.tsx
+++ src/components/features/notes/NoteCard/NoteCard.tsx
@@ -1,6 +1,11 @@
-export function NoteCard() {
+import {Button} from "../../../ui/Button/Button.tsx";
+import {TagBadge} from "../../tags/TagBadge/TagBadge.tsx";
+import type {NoteCardProps} from "./NoteCardProps.ts";
+
+export function NoteCard(props: Readonly<NoteCardProps>) {
     return (
-        <>
-        </>
+        <div className="card card-bordered bg-base-100 shadow-xl">
+            {/*<figure className="px-10 pt-10">*/}
+        </div>
     );
 }

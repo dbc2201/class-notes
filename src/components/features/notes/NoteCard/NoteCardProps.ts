d8902abde46d8522608aa08cb1cc253216a797a5
rohit3171999 <rohit3171999@gmail.com>
2026-01-01 14:30:47 +0530

    feat: Add NoteCard component and props definition
    - Create NoteCard.tsx with basic structure
    - Create NoteCardProps.ts with Note and NoteCardProps interfaces

---
+++ src/components/features/notes/NoteCard/NoteCardProps.ts
@@ -0,0 +1,15 @@
+export interface Note {
+    id: string;
+    title: string;
+    content: string;
+    author: string;
+    createdAt: string;
+    tag: string;
+}
+
+export interface NoteCardProps {
+    note: Note;
+    onEdit: (noteId: string) => void;
+    onDelete: (noteId: string) => void;
+    onClick: (noteId: string) => void;
+}

e1203588adbbd5e8a94f1ed96fd7ef1cfa57e921
rohit3171999 <rohit3171999@gmail.com>
2026-01-02 22:33:13 +0530

    feat(Dropdown): implement Dropdown component with trigger and menu

--- src/components/ui/Dropdown/Dropdown.tsx
+++ src/components/ui/Dropdown/Dropdown.tsx
@@ -1,5 +1,23 @@
-export function Dropdown() {
-    return (
-        <></>
-    );
+import type {DropdownProps} from "./DropdownProps.ts";
+
+export function Dropdown(props: Readonly<DropdownProps>) {
+    return (<div className="dropdown">
+        {/*Trigger*/}
+        <button type="button" className="btn">
+            {props.trigger}
+        </button>
+        {/*Menu*/}
+        <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box">
+            {props.items.map((item) => (<li key={item.value}>
+                    <button
+                        type="button"
+                        className="w-full text-left"
+                        onClick={item.onClick}
+                    >
+                        {item.label}
+                    </button>
+                </li>))}
+
+        </ul>
+    </div>);
 }
